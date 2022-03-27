151ed08af8021150d09ce73dfc84bd078d4ad3b7
Gabor Szabo <gabor@szabgab.com>
2022-03-27 14:49:39 +0300

    add words

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash -xe
 
 #python convert.py English Ladino
-`dirname $0`/LibreLingo/apps/tools/librelingo_tools/lili.py --export --html `dirname $0`/html/ --rel course
+time `dirname $0`/LibreLingo/apps/tools/librelingo_tools/lili.py --export --html `dirname $0`/html/ --rel course
